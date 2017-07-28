dc4a474c753015e08ee0b48f42f4a3d78aa86214
kennethphough <kenneth@egtech.us>
2017-07-28 10:23:29 -0500

    Initial commit

---
+++ init_db.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+
+# Initialises SQLite database to store reference and sample data
+
+# Kenneth P. Hough
+# kenneth AT egtech DOT us
+# License: GNU GPL 3.0
+
+use DBI;
+use strict;
+use warnings;
+
+my ($file) = @ARGV;
+
+if (not defined $file) {
+        die "No database file specified.\n";
+}
+
+my $driver = "SQLite";
+my $database = $file;
+my $dsn = "DBI:$driver:$database";
+my $user = "";
+my $password = "";
+my $dbh = DBI->connect($dsn, $user, $password, {RaiseError => 1}) or die $DBI::errstr;
+
+print "+ successfully connected to db\n";
+
+# initialise DB by creating table if it doesn't exist
+my $stmt = qq(CREATE TABLE IF NOT EXISTS reference_table(
+	id INT PRIMARY KEY NOT NULL,
+	accession_id CHAR(16) NOT NULL,
+	gene_symbol CHAR(16) NOT NULL,
+	gene_name CHAR(64) NOT NULL,
+	chromosome char(2) NOT NULL,
+	species INT NOT NULL,
+	pathway_accession CHAR(16) NOT NULL,
+	pathway_name TEXT NOT NULL,
+	evidence_id CHAR(16) NOT NULL,
+	evidence_type CHAR(16) NOT NULL,
+	panther_subfamily_id CHAR(16),
+	panther_subfamily_name TEXT););
+
+my $r = $dbh->do($stmt);
+if ($r < 0) {
+	print $DBI::errstr;
+} else {
+	print "+ successfuly created reference_table\n";
+}
+$dbh->disconnect();
+
